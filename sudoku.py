5b2f3483dea8fe2e6c70f3fe21a276e8848c3964
ebanez8 <zhou.evannn@gmail.com>
2024-12-01 09:41:28 -0500

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -19,3 +19,60 @@ class Check:
             column = [board[j][i] for j in range(9) if board[j][i] != "."]
             if len(column) != len(set(column)):
                 return False
+
+        for j in range(3):
+            for l in range(3):
+                subgrid = [
+                    board[j * 3 + m][l * 3 + n]
+                    for m in range(3)
+                    for n in range(3)
+                    if board[j * 3 + m][l * 3 + n] != "."
+                ]
+                if len(subgrid) != len(set(subgrid)):
+                    return False
+
+        return True
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        def isValid(board, row, col, num):
+            for i in range(9):
+                if board[row][i] == num or board[i][col] == num:
+                    return False
+
+            startRow, startCol = 3 * (row // 3), 3 * (col // 3)
+            for i in range(startRow, startRow + 3):
+                for j in range(startCol, startCol + 3):
+                    if board[i][j] == num:
+                        return False
+
+            return True
+
+        def backtrack():
+            for row in range(9):
+                for col in range(9):
+                    if board[row][col] == '.':
+                        for num in map(str, range(1, 10)):
+                            if isValid(board, row, col, num):
+                                board[row][col] = num
+                                if backtrack():
+                                    return True
+                                board[row][col] = '.'
+                        return False
+            return True
+
+        backtrack()
+
+# Main execution
+input_handler = Input()
+board = input_handler.takeinput()
+
+check = Check()
+if not check.isValidSudoku(board):
+    print("The input Sudoku board is invalid!")
+else:
+    solver = Solution()
+    solver.solveSudoku(board)
+    print("Solved Sudoku board:")
+    for row in board:
+        print(" ".join(row))
